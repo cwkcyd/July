c64fafd4d937355b48206a40618dcf4b6f849e87
scheshan <5373827@qq.com>
2017-07-02 11:45:31 +0800

    Add modules

---
+++ src/July/Modules/JulyModule.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace July.Modules
+{
+    public abstract class JulyModule
+    {
+        public virtual void PreInitialize()
+        {
+
+        }
+
+        public virtual void Initialize(IServiceCollection services)
+        {
+
+        }
+
+        public virtual void Load()
+        {
+
+        }
+    }
+}
