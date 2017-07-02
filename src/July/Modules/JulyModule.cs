2e80eba1c6a10464c5fd0d5d9ba995cc6bef4cbe
scheshan <5373827@qq.com>
2017-07-02 15:30:13 +0800

    Edit methods in JulyModule

--- src/July/Modules/JulyModule.cs
+++ src/July/Modules/JulyModule.cs
@@ -1,25 +1,22 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using July.Ioc;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Autofac;
 
 namespace July.Modules
 {
     public abstract class JulyModule
     {
-        public virtual void PreInitialize()
+        public virtual void Initialize(IocBuilder builder)
         {
-
+            
         }
 
-        public virtual void Initialize(IServiceCollection services)
+        public virtual void Load(IIocContainer container)
         {
-
-        }
-
-        public virtual void Load()
-        {
-
+            
         }
     }
 }
