30545bef82d5903914cc58dfad7d6c9a32b6da4c
scheshan <5373827@qq.com>
2017-07-05 20:36:22 +0800

    Add EntityFrameworkCore module

---
+++ src/July.Modules.EntityFrameworkCore/EntityFrameworkCoreModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace July.Modules.EntityFrameworkCore
+{
+    [DependOn(typeof(JulyKernelModule))]
+    public class EntityFrameworkCoreModule : JulyModule
+    {
+
+    }
+}
---
+++ src/July.Modules.EntityFrameworkCore/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace July.Modules.EntityFrameworkCore.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddJulyDbContext<TDbContext>(this IServiceCollection services)
+        {
+            
+        }
+    }
+}
